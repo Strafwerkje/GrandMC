c92fa53d5f5bbcb028e07adf5663b853b8b5ddda
Strafwerkje <66158426+Strafwerkje@users.noreply.github.com>
2020-08-26 17:48:20 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,13 +2,13 @@ const botConfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
 
-    var help = new discord.MessageEmbed()
+    var helpEmbed = new discord.MessageEmbed()
     .setColor("#1f2e18")
     .setTitle("Dit zijn al onze help commands!")
     .setDescription("**Player commands:** /n ?help /n > Krijg dit command te zien! /n ?ticket /n > Maak een ticket aan om hulp te krijgen van ons support team! /n")
     .setTimestamp()
 
-    message.channel.send(help);
+    message.channel.send(helpEmbed);
 }
 module.exports.help = {
     name: "help",
