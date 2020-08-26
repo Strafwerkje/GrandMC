52e94245ae3c712741feb35bee25ee8fcc56e284
Strafwerkje <66158426+Strafwerkje@users.noreply.github.com>
2020-08-26 17:55:31 +0200

    Create help1.js

---
+++ commands/help1.js
@@ -0,0 +1,19 @@
+const discord = require("discord.js");
+const botConfig = require("../botconfig.json");
+module.exports.run = async (client, message, args) => {
+    var help = new discord.MessageEmbed()
+ 
+    .setTitle(`**● Help commands:**`)
+    .setTimestamp()
+    .setColor("#0099ff")
+    .setDescription("**● Speler commands: /n ?help /n > Krijg dit command te zien! /n")
+
+        return message.channel.send(embed);
+   
+
+}
+ 
+module.exports.help = {
+    name: "help",
+    description: "dit is Alle Serverinformatie wat u op dit moment nodig Heeft."
+}
