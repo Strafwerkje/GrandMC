c3faf5ae177996f7e5a5806512627ea82f8c2b22
Strafwerkje <66158426+Strafwerkje@users.noreply.github.com>
2020-08-26 20:06:09 +0200

    send

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,19 @@
+const discord = require("discord.js");
 const botConfig = require("../botconfig.json");
-
-module.exports.run = async (bot, message, args) => {
-
-    var helpEmbed = new discord.MessageEmbed()
-    .setColor("#1f2e18")
-    .setTitle("Dit zijn al onze help commands!")
-    .setDescription("**Player commands:** /n ?help /n > Krijg dit command te zien! /n ?ticket /n > Maak een ticket aan om hulp te krijgen van ons support team! /n")
+module.exports.run = async (client, message, args) => {
+    var help = new discord.MessageEmbed()
+ 
+    .setTitle(`**● Help commands:**`)
     .setTimestamp()
+    .setColor("#1f2e18")
+    .setDescription("**Speler commands:** \n ?help \n > Krijg dit command te zien! \n ?idee \n > Laat een idee achter! \n ?ticket \n > Maak een ticket aan! \n ?serverinfo \n > Krijg alle informatie over onze  server! \n \n **Moderation Commands:** \n ?ban \n > Ban iemand van de server! \n ?kick \n > Kick iemand van de server! \n ?mute \n > Zorg dat iemand niet meer kan praten! \n ?clear \n Verwijderer meer berichten in 1 keer! \n ?warn \n > Geef iemand een waarschuwing in de server! \n ?close \n > Sluit een ticket van iemand! \n ?giveaway \n > Maak een giveaway aan!")
 
-    message.channel.send(helpEmbed);
+        return message.channel.send(help);
+   
+
 }
+ 
 module.exports.help = {
     name: "help",
-    description:"geef dit en help command"
-
+    description: "dit is Alle Serverinformatie wat u op dit moment nodig Heeft."
 }
